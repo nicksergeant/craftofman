268458102d81ac41da1027515ba9eaf8553dc66d
Nick Sergeant <nick@nicksergeant.com>
2012-09-09 21:11:16 -0400

    Ignore static

--- static/css/compile-css.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat media/css/style.css \
-    > media/css/craftofman.css
--- static/js/compile-js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat media/js/jquery.slabtext.min.js \
-    media/js/script.js \
-    > media/js/craftofman.js
